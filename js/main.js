1274030a3332b8fb44b9303deebef44de3b05ba1
Minh Thang <chethet20@gmail.com>
2021-10-14 23:59:03 +0700

    CV Add cursor effect, update cv file, update intro

--- js/main.js
+++ js/main.js
@@ -36,6 +36,43 @@
       $('body').addClass('loaded');
     }, 1000);
 
+    // UPDATE: I was able to get this working again... Enjoy!
+
+    var cursor = document.querySelector('.cursor');
+    var cursorinner = document.querySelector('.cursor2');
+    var a = document.querySelectorAll('a');
+
+    document.addEventListener('mousemove', function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+    });
+
+    document.addEventListener('mousemove', function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursorinner.style.left = x + 'px';
+      cursorinner.style.top = y + 'px';
+    });
+
+    document.addEventListener('mousedown', function () {
+      cursor.classList.add('click');
+      cursorinner.classList.add('cursorinnerhover')
+    });
+
+    document.addEventListener('mouseup', function () {
+      cursor.classList.remove('click')
+      cursorinner.classList.remove('cursorinnerhover')
+    });
+
+    a.forEach(item => {
+      item.addEventListener('mouseover', () => {
+        cursor.classList.add('hover');
+      });
+      item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
+      });
+    })
   });
 
 
@@ -152,30 +189,30 @@
   // ------------------------------------------------------------------------------ //
 
 
-$('#screenshot-carusel').owlCarousel({
+  $('#screenshot-carusel').owlCarousel({
     loop: true,
     responsiveClass: true,
     nav: true,
-    margin: 5,    
+    margin: 5,
     autoplayTimeout: 4000,
     smartSpeed: 500,
     center: true,
     navText: ['<span class="ti ti-angle-left"></span>', '<span class="ti ti-angle-right"></span>'],
     responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 3
-        },
-        1200: {
-            items: 5
-        }
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3
+      },
+      1200: {
+        items: 5
+      }
     }
-});
+  });
 
 
-  
+
 
 
 
@@ -239,18 +276,18 @@ $('#screenshot-carusel').owlCarousel({
   // ------------------------------------------------------------------------------ //
 
 
-	$(".skill_main").each(function() {
-    $(this).waypoint(function() {
-        var progressBar = $(".progress-bar");
-        progressBar.each(function(indx){
-            $(this).css("width", $(this).attr("aria-valuenow") + "%")
-        })
+  $(".skill_main").each(function () {
+    $(this).waypoint(function () {
+      var progressBar = $(".progress-bar");
+      progressBar.each(function (indx) {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%")
+      })
     }, {
-        triggerOnce: true,
-        offset: 'bottom-in-view'
+      triggerOnce: true,
+      offset: 'bottom-in-view'
 
     });
-});
+  });
 
 
   // ------------------------------------------------------------------------------ //
