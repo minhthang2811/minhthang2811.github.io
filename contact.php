e4f7f91b5afc675f76abd43ccce3fa5a506e7be6
Minh Thang <chethet20@gmail.com>
2021-11-11 16:02:47 +0700

    CV add email to contact form

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php 
 	if($_POST) {
 
-		$to = "youremail@gmail.com"; // Your email here
+		$to = "minhthang281198@gmail.com"; // Your email here
 		$subject = 'Message from my website'; // Subject message here
 
 	}
